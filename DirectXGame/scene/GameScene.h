6222c2d4f5b12255ade346f08f50be068abbc05e
sora <147121795+sora7716@users.noreply.github.com>
2024-08-18 10:54:54 +0900

    自分で書いた処理をすべて消した

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -8,8 +8,6 @@
 #include "ViewProjection.h"
 #include "WorldTransform.h"
 
-#include "DebugCamera.h"
-
 
 /// <summary>
 /// ゲームシーン
@@ -52,22 +50,4 @@ private: // メンバ変数
 	/// </summary>
 
 public:
-	Sprite* sprite_ = nullptr; // 2Dの描画に使う
-	Model* model_ = nullptr;   // 3Dの描画に使う
-
-	WorldTransform worldTransform_; // ワールドトランスフォーム
-	ViewProjection viewProjection_; // ビュープロジェクション
-
-	DebugCamera* debugCamera_ = nullptr;
-
-private:
-	uint32_t textureHandle2D_ = 0; // テクスチャハンドル
-	uint32_t textureHandle3D_ = 0; // テクスチャハンドル
-
-	uint32_t soundDateHandle_ = 0; // サウンドハンドル
-	uint32_t voiceHandle_ = 0;     // 音声再生ハンドル
-
-	float inputFloat3[3] = {}; // ImGuiで値を入力する変数
-
-	Vector2 screenSize = {WinApp::kWindowHeight, WinApp::kWindowHeight};
 };
