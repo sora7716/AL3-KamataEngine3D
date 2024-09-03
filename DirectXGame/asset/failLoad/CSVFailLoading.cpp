e2f565cdbe81cb7764fcd365acfa8f637f11444f
sora <147121795+sora7716@users.noreply.github.com>
2024-09-04 04:07:10 +0900

    csvファイルの読み込み用のクラスを作成した

---
+++ DirectXGame/asset/failLoad/CSVFailLoading.cpp
@@ -0,0 +1,95 @@
+#include "CSVFailLoading.h"
+#include <fstream>
+#include <iostream>
+//初期化
+void CSVFailLoading::Initialize(){
+	failDataCommands_ = LoadData("csvFail/enemyPop.csv");
+}
+
+//更新
+void CSVFailLoading::Update(){
+	UpdatePopCommand(failDataCommands_,position_,waitTime_);
+}
+
+//位置のゲッター
+std::list<Vector3> CSVFailLoading::GetPosition() {
+	return position_;
+}
+
+//読み込み
+std::stringstream CSVFailLoading::LoadData(std::string failName) {
+	std::stringstream failDataCommands;
+	//ファイルを開く
+	std::ifstream file;
+	file.open(failName);
+	//ファイルの内容を文字列ストリームにコピー
+	failDataCommands << file.rdbuf();
+	file.close();
+
+	return failDataCommands;
+}
+
+//コマンドの更新
+void CSVFailLoading::UpdatePopCommand(std::stringstream& failDataCommands, std::list<Vector3>& position, int32_t waitTime) {
+	static bool isWaiting = false;  // 待機状態を示すフラグ
+
+	// 待機状態の場合、カウンタを減少させる
+	if (isWaiting) {
+		waitTime--;
+		if (waitTime <= 0) {
+			isWaiting = false;  // 待機終了
+		}
+		return;  // 待機中は次のコマンドを実行しない
+	}
+
+	//1行分の文字列を入れる変数
+	std::string line;
+	//コマンドを実行ループ
+	while (getline(failDataCommands, line)) {
+		if (line.empty()) {
+			//空白を飛ばす
+			continue;
+		}
+		//1行分の文字列をストリームに変換して解析し約する
+
+		std::istringstream line_stream(line);//読み込んだ行を文字列ストリームに変換
+		std::string word;//行から取り出した単語を格納する変数
+
+		//,区切りで行の先頭文字を取得
+		getline(line_stream, word, ',');
+		if (word.find("//") == 0) {
+			//コメント行を飛ばす
+			continue;
+		}
+		//POPコマンド
+		if (word.find("POP") == 0) {
+			//x座標
+			getline(line_stream, word, ',');
+			float x = (float)atof(word.c_str());
+
+			//y座標
+			getline(line_stream, word, ',');
+			float y = (float)atof(word.c_str());
+
+			//z座標
+			getline(line_stream, word, ',');
+			float z = (float)atof(word.c_str());
+
+			//敵を発生させる
+			Vector3 newPosition = { x,y,z };
+			position.push_back(newPosition);
+		}
+		else if (word.find("WAIT") == 0) {
+			getline(line_stream, word, ',');
+			//待ち時間
+			int32_t newWaitTime = atoi(word.c_str());
+			//待機時間
+			waitTime = newWaitTime;//どれくらい待機するか
+			std::cout << "waitTime" << waitTime << std::endl;
+			// 待機状態に入る
+			isWaiting = true;
+			//コマンドのループを抜ける
+			break;
+		}
+	}
+}
---
+++ DirectXGame/asset/failLoad/CSVFailLoading.h
@@ -0,0 +1,58 @@
+#pragma once
+#include<cmath>
+#define _USE_MATH_DEFINES
+#include <sstream>
+#include <list>
+#include "Vector3.h"
+
+/// <summary>
+/// CSVファイルを読み込む
+/// </summary>
+class CSVFailLoading{
+public://メンバ関数
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	CSVFailLoading() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~CSVFailLoading() = default;
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+    /// 位置のゲッター
+    /// </summary>
+	std::list<Vector3>GetPosition();
+
+private: // メンバ変数
+
+	/// <summary>
+	/// 読み込み
+	/// </summary>
+	/// <param name="failName">csvファイルの名前</param>
+	std::stringstream LoadData(std::string failName);
+
+	/// <summary>
+	/// コマンドの更新
+	/// </summary>
+	/// <param name="failDataCommands">コマンドを格納しておく変数</param>
+	void UpdatePopCommand(std::stringstream& failDataCommands, std::list<Vector3>& position, int32_t waitTime);
+
+private://メンバ変数
+	std::stringstream failDataCommands_;//ファイルデータを格納しておく変数
+	std::list<Vector3>position_;//データに書いてあった位置を格納しておく変数
+	int32_t waitTime_ = 0;  // 待機時間のカウンタを保持するための変数
+};
+
