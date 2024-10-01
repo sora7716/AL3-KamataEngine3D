91f3e268aeb608f711d3b325d9bda0a77da91ccf
sora <147121795+sora7716@users.noreply.github.com>
2024-10-02 00:28:55 +0900

    セーブ

--- DirectXGame/assets/gameObject/Hexagon/Hexagon.cpp
+++ DirectXGame/assets/gameObject/Hexagon/Hexagon.cpp
@@ -12,7 +12,7 @@ void Hexagon::Initialize(Model* model, ViewProjection* viewProjection) {
 	model_ = move(model);                   // モデルを受け取る
 	viewProjection_ = viewProjection;       // ビュープロジェクションを受け取る
 	polar_.radian = pi_v<float> / 3.0f;     // 60度を弧度法に直した
-	polar_.radius = 1.0f;                   // 半径
+	polar_.radius = 0.9f;                   // 半径
 	polar_.diameter = polar_.radius * 2.0f; // 直径
 	worldTransforms_.resize(kLayer);//層の数を設定
 	parent_ = new WorldTransform();//六角形の親の生成
