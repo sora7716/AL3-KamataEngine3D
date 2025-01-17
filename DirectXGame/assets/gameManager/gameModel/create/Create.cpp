69c4da27a3cedcae7bf3743074c4ab8926ff43f8
Cortesja <146901338+Cortesja@users.noreply.github.com>
2025-01-17 19:58:17 +0900

    PlayerHPバーを表示できるようになった。

--- DirectXGame/assets/gameManager/gameModel/create/Create.cpp
+++ DirectXGame/assets/gameManager/gameModel/create/Create.cpp
@@ -62,16 +62,10 @@ void Create::EndSceneModel() {}
 
 // テクスチャをクリエイト
 void Create::TextureCreate() {
-	//textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Damage.png"));//0
-	//textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Health.png"));//1
-	//textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Waku.png"));//2
-	//textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Health_Enemy.png"));//3
-
-
-	textureHandle_.push_back(TextureManager::Load("ui/LifeBar.png"));//0
-	textureHandle_.push_back(TextureManager::Load("ui/LifeBar.png"));//1
-	textureHandle_.push_back(TextureManager::Load("ui/LifeBar.png"));//2
-	textureHandle_.push_back(TextureManager::Load("ui/LifeBar.png"));//3
+	textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Damage.png"));//0
+	textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Health.png"));//1
+	textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Waku.png"));//2
+	textureHandle_.push_back(TextureManager::Load("ui/LifeBar_Health_Enemy.png"));//3
 }
 
 // モデルのゲッター
